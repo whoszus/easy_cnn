e38aa6ccd530f1b0b8f8cda30f49be963ca44288
tinker <whoszus@yahoo.com>
2019-08-15 16:21:48 +0800

    update: MSEloss 版本进行测试

--- src/pytorch_version/utils.py
+++ src/pytorch_version/utils.py
@@ -24,7 +24,7 @@ c_time = time.strftime("%Y-%m-%d-%H-%M-%S", time.localtime())
 log_f = open("logs/" + c_time + '.log', 'w+')
 
 train_f = 'data/data_1.csv'
-test_f = "data/test_1_8k.csv"
+test_f = "data/data_2.csv"
 embedding = nn.Embedding(728, 16)
 # embedding_time = nn.Embedding(512, 8)
 batch_x = 128
