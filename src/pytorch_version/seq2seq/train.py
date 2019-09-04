f6becd30a5a38f97bd62db51beba5f49ee789c86
tinker <whoszus@yahoo.com>
2019-09-04 10:25:05 +0800

    update : commit logs

--- src/pytorch_version/seq2seq/train.py
+++ src/pytorch_version/seq2seq/train.py
@@ -231,7 +231,7 @@ def main():
     parser.add_argument('-embs_share_weight', action='store_true')
     parser.add_argument('-proj_share_weight', action='store_true')
 
-    parser.add_argument('-log', default=None)
+    parser.add_argument('-log', default='log/logs.log')
     parser.add_argument('-save_model', default='trained_9')
     parser.add_argument('-save_mode', type=str, choices=['all', 'best'], default='best')
 
