70d70d6db921c48e659496a1e0a9e14e9b78c260
tinker <whoszus@yahoo.com>
2019-09-08 15:27:36 +0800

    update : commit to test

--- src/pytorch_version/seq2seq/train.py
+++ src/pytorch_version/seq2seq/train.py
@@ -220,10 +220,10 @@ def main():
     # parser.add_argument('-torch_save_data', default='data/origin/2018-06-01#2018-06-15.pt')
     parser.add_argument('-save_model', default='module/2018-06-01#2018-06-15#cc.pt')
     parser.add_argument('-start_time', default='2018-06-01')
-    parser.add_argument('-end_time', default='2018-06-15')
+    parser.add_argument('-end_time', default='2018-06-2')
 
     parser.add_argument('-epoch', type=int, default=60)
-    parser.add_argument('-batch_size', type=int, default=64)
+    parser.add_argument('-batch_size', type=int, default=1)
 
     # parser.add_argument('-d_word_vec', type=int, default=512)
     parser.add_argument('-d_model', type=int, default=512)
